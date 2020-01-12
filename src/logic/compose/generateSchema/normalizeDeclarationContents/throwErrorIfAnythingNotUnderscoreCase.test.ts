6c4f2a475cac64413c3d8f3af0ea71b88a626c0b
UladKasach <nappa300@gmail.com>
2020-01-12 16:37:23 -0600

    feat(namingConventions): ensure all entity and property names are in underscore_case

---
+++ src/logic/compose/generateSchema/normalizeDeclarationContents/throwErrorIfAnythingNotUnderscoreCase.test.ts
@@ -0,0 +1,46 @@
+import { prop } from '../../../../contract/module';
+import { Entity } from '../../../../types';
+import { throwErrorIfAnythingNotUnderscoreCase } from './throwErrorIfAnythingNotUnderscoreCase';
+
+describe('throwErrorIfAnythingNotUnderscoreCase', () => {
+  it('should throw an error if entity name is not underscore case', () => {
+    const treeHouse = new Entity({ name: 'treeHouse', properties: { name: prop.VARCHAR(255) }, unique: ['name'] });
+    try {
+      throwErrorIfAnythingNotUnderscoreCase({ entity: treeHouse });
+      throw new Error('should not reach here');
+    } catch (error) {
+      expect(error.message).toEqual(
+        "the name of entity 'treeHouse' must be in underscore_case, as that is the standard in sql",
+      );
+    }
+  });
+  it('should throw an error if any property of an entity is not in underscore case', () => {
+    const treeHouse = new Entity({
+      name: 'tree_house',
+      properties: {
+        name: prop.VARCHAR(255),
+        dateBuilt: prop.DATETIME(6),
+      },
+      unique: ['name'],
+    });
+    try {
+      throwErrorIfAnythingNotUnderscoreCase({ entity: treeHouse });
+      throw new Error('should not reach here');
+    } catch (error) {
+      expect(error.message).toEqual(
+        "property 'dateBuilt' of entity 'tree_house' must be in underscore_case, as that is the standard in sql",
+      );
+    }
+  });
+  it('should not throw error if everything is in underscore case', () => {
+    const treeHouse = new Entity({
+      name: 'tree_house',
+      properties: {
+        name: prop.VARCHAR(255),
+        date_built: prop.DATETIME(6),
+      },
+      unique: ['name'],
+    });
+    throwErrorIfAnythingNotUnderscoreCase({ entity: treeHouse });
+  });
+});
---
+++ src/logic/compose/generateSchema/normalizeDeclarationContents/throwErrorIfAnythingNotUnderscoreCase.ts
@@ -0,0 +1,23 @@
+import { Entity } from '../../../../types';
+
+/*
+  sql is, as a language, underscore_case based.
+
+  we should follow precedent, therefore, and keep using underscore_case
+*/
+const underscoreCaseRegex = new RegExp('^[a-z]+(_[a-z]+)*$');
+export const throwErrorIfAnythingNotUnderscoreCase = ({ entity }: { entity: Entity }) => {
+  // throw error if entity name is not underscore_case
+  if (!underscoreCaseRegex.test(entity.name)) {
+    throw new Error(`the name of entity '${entity.name}' must be in underscore_case, as that is the standard in sql`);
+  }
+
+  // throw error if any property name is not underscore_case
+  Object.keys(entity.properties).forEach((name) => {
+    if (!underscoreCaseRegex.test(name)) {
+      throw new Error(
+        `property '${name}' of entity '${entity.name}' must be in underscore_case, as that is the standard in sql`,
+      );
+    }
+  });
+};
