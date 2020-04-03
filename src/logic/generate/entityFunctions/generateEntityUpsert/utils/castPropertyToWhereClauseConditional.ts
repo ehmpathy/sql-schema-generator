fb1f47378f7061ea536d1b392a1b4a99b6229fbf
UladKasach <nappa300@gmail.com>
2020-04-03 05:05:47 -0500

    fix(upsert): enforce case sensitivity with collation/charset only, drop BINARY comparison per #26

--- src/logic/generate/entityFunctions/generateEntityUpsert/utils/castPropertyToWhereClauseConditional.ts
+++ src/logic/generate/entityFunctions/generateEntityUpsert/utils/castPropertyToWhereClauseConditional.ts
@@ -6,7 +6,7 @@ export const castPropertyToWhereClauseConditional = ({ name, definition }: { nam
   const columnName = castPropertyToColumnName({ name, definition }); // no change
   const columnValueReference = castPropertyToTableColumnValueReference({ name, definition });
   return [
-    `AND (${columnName} = BINARY ${columnValueReference}`, // use binary comparison to ensure case sensitivity (even though column may be case sensitive, mysql will still "help us out" by defaulting to case insensitive)
+    `AND (${columnName} = ${columnValueReference}`,
     definition.nullable ? ` OR (${columnName} IS null AND ${columnValueReference} IS null)` : '', // NULL != NULL, so special check if field is nullable
     ')',
   ].join('');
